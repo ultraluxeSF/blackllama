eb2c1e59cbb172f12998827cd15fee02e32852a3
ultraluxe <ultraluxe43@gmail.com>
2022-05-03 21:29:46 +0500

    upd3May

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,22 @@
 import { applyMiddleware, createStore, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
 //import data from "./data";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 
 
-const initialState = {};
+const initialState = {
+    cart: {
+        //set default cartItems value & convert it to array
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
+    }
+};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
